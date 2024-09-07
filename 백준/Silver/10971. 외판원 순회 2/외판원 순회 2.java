5162282cf7d99330440f281b679af67f6e5f47a8
DonghyeonKwon <93506933+DonghyeonKwon@users.noreply.github.com>
2024-09-07 20:43:12 +0900

    [Silver II] Title: 외판원 순회 2, Time: 228 ms, Memory: 12016 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -7,9 +7,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		//입력
 		n = Integer.parseInt(br.readLine());
 		map = new int[n][n];
 		
@@ -19,49 +17,26 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		int[] arr = new int[n];
-
+        
 		for(int i = 0; i < n; i++) {
-			arr[i] = i;
+			go((1 << i), i, i, 1, 0);
 		}
-	
-		do {
-			int sum = 0;
-			boolean flag = true;
-			for(int i = 0; i < n; i++) {
-				if(map[arr[i]][arr[(i+1)%n]] == 0) {
-					flag = false;
-					break;
-				}
-				sum += map[arr[i]][arr[(i+1)%n]];
-			}
-			if(flag && res > sum) res = sum;
-		} while(nextPermutation(arr));
 		
-		bw.write(Integer.toString(res));
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.println(res);
 	}
 	
-	static boolean nextPermutation(int[] arr) {
-		int N = arr.length;
+	static void go(int ecnt, int s, int prev, int cnt, int sum) {
+		if(cnt == n) {
+			if(ecnt != (1 << n) - 1) return;
+            if(map[prev][s] == 0)return;
+			if(res > sum + map[prev][s]) res = sum + map[prev][s];
+			return;
+		}
 		
-		int i = N - 1;
-		while(i > 0 && arr[i-1] > arr[i]) i--;
-		
-		if(i == 0) return false;
-		
-		int j = N - 1;
-		
-		while(arr[i-1] > arr[j]) j--;
-		int tmp = arr[i-1];
-		arr[i-1]=arr[j];
-		arr[j] = tmp;
-		
-		Arrays.sort(arr, i, N);
-		
-		return true;
+		for(int i = 0; i < n; i++) {
+			if((ecnt & (1 << i)) > 0) continue;
+			if(map[prev][i] == 0) continue;
+			go(ecnt | (1 << i), s, i, cnt+1, sum + map[prev][i]);
+		}
 	}
 }
