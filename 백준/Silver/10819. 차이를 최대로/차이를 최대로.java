e8fb4a2a8f7d50d9b05c889ad53867da2b39fdc9
DonghyeonKwon <93506933+DonghyeonKwon@users.noreply.github.com>
2024-12-05 03:00:54 +0900

    [Silver II] Title: 차이를 최대로, Time: 112 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -3,7 +3,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, max = -1;
-    static int[] arr, choosed;
+    static int[] arr;
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
@@ -12,7 +12,6 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
         arr = new int[N];
-        choosed = new int[N];
         visited = new boolean[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,33 +19,29 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        combi(0);
+        for(int i = 0; i < N; i++) {
+            visited[i] = true;
+            combi(arr[i], 1, 0);
+            visited[i] = false;
+        }
 
         bw.write(Integer.toString(max));
         bw.flush();
     }
 
-    static void combi(int idx){
-        if(idx == N){
-            cal();
+    static void combi(int prev, int cnt, int sum){
+        if(cnt == N){
+            max = Math.max(max,sum);
             return;
         }
 
         for(int i = 0; i < N; i++){
             if(visited[i]) continue;
             visited[i] = true;
-            choosed[idx] = i;
-            combi(idx+1);
+            int a = Math.abs(prev - arr[i]);
+            combi(arr[i], cnt+1, sum+a);
             visited[i] = false;
         }
     }
 
-    static void cal() {
-        int sum = 0;
-
-        for(int i = 0; i < N-1; i++) {
-            sum += Math.abs(arr[choosed[i]] - arr[choosed[i + 1]]);
-        }
-        max = Math.max(max, sum);
-    }
 }
