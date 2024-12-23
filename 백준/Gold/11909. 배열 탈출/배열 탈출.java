665de20beeb412454dd39c9535c4bdd62b0d2da6
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2024-12-24 06:34:07 +0900

    [Gold V] Title: 배열 탈출, Time: 4800 ms, Memory: 358592 KB -BaekjoonHub

---
+++ 백준/Gold/11909. 배열 탈출/배열 탈출.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	static int n;
+	static int[][] map;
+	
+	static int[] dy = {1, 0};
+	static int[] dx = {0, 1};
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		map = new int[n+1][n+1];
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
+		
+		int[][] visited = new int[n+1][n+1];
+		for(int i = 1; i <= n; i++) {
+			Arrays.fill(visited[i], 999999999);
+		}
+		PriorityQueue<Pos> pq = new PriorityQueue<>();
+		pq.add(new Pos(1, 1, 0));
+		visited[1][1] = 0;
+		
+		while(!pq.isEmpty()) {
+			Pos p = pq.poll();
+			int y = p.y;
+			int x = p.x;
+			int won = p.won;
+			
+			for(int i = 0; i < 2; i++) {
+				int ny = y + dy[i];
+				int nx = x + dx[i];
+				int nwon = won;
+				
+				if(ny < 1 || nx < 1 || ny > n || nx > n) continue;
+				
+				if(map[ny][nx] >= map[y][x]) {
+					nwon += (map[ny][nx] - map[y][x] + 1);
+				}
+				
+				if(nwon >= visited[ny][nx]) continue;
+				
+				visited[ny][nx] = nwon;
+				pq.offer(new Pos(ny, nx, nwon));
+			}
+		}
+		
+		System.out.println(visited[n][n]);
+	}
+	
+	static class Pos implements Comparable<Pos>{
+		int y, x, won;
+		Pos(int y, int x, int won){
+			this.y = y;
+			this.x = x;
+			this.won = won;
+		}
+		@Override
+		public int compareTo(Pos o) {
+			return Integer.compare(this.won, o.won);
+		}
+	}
+}
