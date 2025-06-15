3701ee8477c78be581c35053dbdb4ca51e5a7e23
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2025-06-15 17:27:41 +0900

    [Gold IV] Title: 빙산, Time: 1296 ms, Memory: 300300 KB -BaekjoonHub

---
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -0,0 +1,132 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, -1, 0, 1};
+
+    static int[][] map;
+    static boolean[][] visited;
+    static Queue<int[]> melted = new ArrayDeque<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int time = 0;
+        while(true) {
+            visited = new boolean[n][m];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (visited[i][j]) continue;
+                    if (map[i][j] != 0) continue;
+
+                    zero_bfs(i, j);
+                }
+            }
+
+            while (!melted.isEmpty()) {
+                int[] pos = melted.poll();
+                int y = pos[0];
+                int x = pos[1];
+
+                if(map[y][x] == 0) continue;
+
+                map[y][x]--;
+            }
+
+            time++;
+
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (map[i][j] == 0) continue;
+                    if (visited[i][j]) continue;
+
+                    count_bfs(i, j);
+                    cnt++;
+                }
+            }
+            
+            if(cnt > 1) break;
+
+            if(cnt == 0) {
+                time = 0;
+                break;
+            }
+        }
+
+        bw.write(Integer.toString(time));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+
+    static void zero_bfs(int y, int x) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{y, x});
+        visited[y][x] = true;
+
+        while(!q.isEmpty()){
+            int[] pos = q.poll();
+            y = pos[0];
+            x = pos[1];
+
+            for(int d = 0; d < 4; d++) {
+                int ny = y + dy[d];
+                int nx = x + dx[d];
+
+                if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+                if(visited[ny][nx]) continue;
+
+                if(map[ny][nx] > 0) {
+                    melted.offer(new int[]{ny, nx});
+                    continue;
+                }
+
+                visited[ny][nx] = true;
+                q.offer(new int[]{ny, nx});
+            }
+        }
+    }
+
+    static void count_bfs(int y, int x){
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{y, x});
+        visited[y][x] = true;
+
+        while(!q.isEmpty()) {
+            int[] pos = q.poll();
+            y = pos[0];
+            x = pos[1];
+
+            for(int d = 0; d < 4; d++) {
+                int ny = y + dy[d];
+                int nx = x + dx[d];
+
+                if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+                if(visited[ny][nx]) continue;
+                if(map[ny][nx] == 0) continue;
+
+                q.offer(new int[]{ny,nx});
+                visited[ny][nx] = true;
+            }
+        }
+    }
+}
