161b153122c9c7753c14553e1810dd7453039e2b
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-01-06 17:55:41 +0900

    [Gold IV] Title: 미로만들기, Time: 112 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static boolean[][] map;
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, -1, 0, 1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        map = new boolean[n+1][n+1];
+        for(int i = 1; i <= n; i++) {
+            String input = br.readLine();
+            for(int j = 0; j < n; j++) {
+                map[i][j+1] = input.charAt(j) == '1';
+            }
+        }
+
+        System.out.print(dijkstra(1, 1));
+    }
+
+    static int dijkstra(int y, int x) {
+        PriorityQueue<Info> pq = new PriorityQueue<>();
+        boolean[][] visited = new boolean[n+1][n+1];
+        pq.add(new Info(y, x, 0, 0));
+        visited[y][x] = true;
+
+        while(!pq.isEmpty()) {
+            Info now = pq.poll();
+            
+            if(now.y == n && now.x == n) {
+                return now.cost;
+            }
+
+            for(int i = 0; i < 4; i++) {
+                int ny = now.y + dy[i];
+                int nx = now.x + dx[i];
+
+                if(ny < 1 || nx < 1 || ny > n || nx > n) continue;
+                if(visited[ny][nx]) continue;
+
+                int ndis = now.dis + 1;
+                int ncost = now.cost;
+
+                if(!map[ny][nx]) {
+                    ncost += 1;
+                }
+                visited[ny][nx] = true;
+                pq.add(new Info(ny, nx, ndis, ncost));
+            }
+        }
+
+        return  -1;
+    }
+
+    static class Info implements Comparable<Info> {
+        int y, x, dis, cost;
+
+        Info(int y, int x, int dis, int cost){
+            this.y = y;
+            this.x = x;
+            this.dis = dis;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Info o) {
+            if(this.cost == o.cost) {
+                return this.dis - o.dis;
+            }
+            return this.cost - o.cost;
+        }
+    }
+}
