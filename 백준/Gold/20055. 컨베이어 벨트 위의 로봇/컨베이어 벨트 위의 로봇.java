4369284354e4a37db56bebec038893e5f70797d1
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-02-08 14:42:07 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 176 ms, Memory: 15068 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -2,10 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, k, step = 1, broken = 0;
-    static int[] belts;
-    static boolean[] robotCheck;
-    static Queue<Integer> robots;
+    static int n, k, step = 0, broken = 0;
+    static Belt[] belts;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,12 +12,10 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        belts = new int[2 * n + 1];
-        robotCheck = new boolean[2 * n + 1];
-        robots = new ArrayDeque<>();
+        belts = new Belt[2 * n + 1];
         st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= 2 * n; i++) {
-            belts[i] = Integer.parseInt(st.nextToken());
+            belts[i] = new Belt(Integer.parseInt(st.nextToken()));
         }
 
         simulation();
@@ -28,80 +24,50 @@ public class Main {
     public static void simulation() {
 
         while(true) {
+            step++;
             //1
             turn();
-
             //2
             moveRobot();
-
             //3
-            if(belts[1] != 0) {
-                robots.add(1);
-                belts[1] -= 1;
-                if(belts[1] == 0) {
+            if(belts[1].hp >= 1) {
+                belts[1].robot = true;
+                belts[1].hp -= 1;
+                if(belts[1].hp == 0) {
                     broken++;
                 }
             }
 
             //4
             if(broken >= k) break;
-            step++;
         }
 
         System.out.print(step);
     }
 
     public static void turn() {
-        int belt = belts[2 * n];
-
         for(int i = 2 * n; i > 1; i--) {
+            Belt temp = belts[i];
             belts[i] = belts[i-1];
-        }
-        belts[1] =  belt;
-
-        Queue<Integer> temp = new ArrayDeque<>();
-
-        while(!robots.isEmpty()) {
-            int robot = robots.poll();
-            robotCheck[robot] = false;
-
-            int nextIdx = robot + 1;
-
-            if(nextIdx == n) continue;
-            if(nextIdx > 2 * n) nextIdx = 1;
-
-            robotCheck[nextIdx] = true;
-            temp.add(nextIdx);
+            belts[i-1] = temp;
         }
 
-        robots = temp;
+        belts[n].robot = false;
     }
 
     static void moveRobot() {
-        Queue<Integer> temp = new ArrayDeque<>();
-
-        while(!robots.isEmpty()) {
-            int robot = robots.poll();
-
-            int nextIdx = robot + 1;
-            if(nextIdx > 2 * n) {
-                nextIdx = 1;
-            }
-
-            if(!robotCheck[nextIdx] && belts[nextIdx] >= 1) {
-                belts[nextIdx] -= 1;
-                if(belts[nextIdx] == 0) broken++;
-                robotCheck[robot] = false;
-
-                if(nextIdx == n) continue;
-                robotCheck[nextIdx] = true;
-                temp.add(nextIdx);
-            } else {
-                temp.add(robot);
+        for(int i = n - 1; i >= 1; i--) {
+            if(!belts[i].robot) continue;
+            if(belts[i+1].hp >= 1 && !belts[i+1].robot) {
+                belts[i].robot = false;
+                belts[i+1].hp -= 1;
+                if(belts[i+1].hp == 0) {
+                    broken += 1;
+                }
+                if(i + 1 == n) continue;
+                belts[i+1].robot = true;
             }
         }
-
-        robots = temp;
     }
 
     static class Belt {
