e1810154a49348f6754783e3d9fe9557bdb2618e
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-02-10 16:41:54 +0900

    [Gold IV] Title: 최단경로, Time: 568 ms, Memory: 109788 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,82 +2,65 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(br.readLine());
-		
-		List<Pos>[] list = new ArrayList[n+1];
-		for(int i = 1; i <= n; i++) {
-			list[i] = new ArrayList<>();
-		}
-		
-		for(int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			
-			list[s].add(new Pos(e, w));
-//			list[e].add(new Pos(s, w));
-		}
-		
-		PriorityQueue<Pos> pq = new PriorityQueue<>();
-		
-		pq.offer(new Pos(k, 0));
-		int cnt = 0;
-		int[] minCost = new int[n+1];
-		boolean[] visited = new boolean[n+1];
-		Arrays.fill(minCost, Integer.MAX_VALUE);
-		minCost[k] = 0;
-		
-		while(!pq.isEmpty()) {
-//			System.out.println(Arrays.toString(minCost));
-			Pos p = pq.poll();
-			int e = p.e;
-			int w = p.w;
-			
-			if(visited[e]) continue;
-			visited[e] = true;
-			cnt++;
-			if(cnt == n) break;
-			
-			for(Pos next : list[e]) {
-				if(visited[next.e]) continue;
-				if(minCost[next.e] > minCost[e] + next.w) {
-					minCost[next.e] = minCost[e] + next.w;
-					pq.add(new Pos(next.e, minCost[next.e]));
-				}
-			}
-		}
-		
-		for(int i = 1; i <= n; i++) {
-			if(minCost[i] == Integer.MAX_VALUE) {
-				bw.write("INF\n");
-				continue;
-			}
-			bw.write(Integer.toString(minCost[i]));
-			bw.append('\n');
-		}
-		
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-	
-	static class Pos implements Comparable<Pos>{
-		int e, w;
-		Pos(int e, int w){
-			this.e = e;
-			this.w = w;
-		}
-		@Override
-		public int compareTo(Pos o) {
-			return this.w - o.w;
-		}
-	}
+    static int INF = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int s = Integer.parseInt(br.readLine());
+
+        List<int[]>[] list = new ArrayList[n+1];
+        for(int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            list[a].add(new int[]{b, c});
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a ,b) -> a[1] - b[1]
+        );
+        boolean[] visited = new boolean[n+1];
+        int[] values = new int[n+1];
+        int cnt = 0;
+        Arrays.fill(values, Integer.MAX_VALUE);
+        values[s] = 0;
+        pq.add(new int[]{s, 0});
+
+        while(!pq.isEmpty()) {
+            int[] arr = pq.poll();
+            int now = arr[0];
+            int value = arr[1];
+
+            if(visited[now]) continue;
+            cnt++;
+            visited[now] = true;
+
+            if(cnt == n) break;
+
+            for(int[] next : list[now]) {
+                if(!visited[next[0]] && values[next[0]] > value + next[1]) {
+                    values[next[0]] = value + next[1];
+                    pq.add(new int[]{next[0], values[next[0]]});
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= n; i++) {
+            sb.append(values[i] == INF ? "INF" : values[i]).append('\n');
+        }
+        System.out.print(sb);
+    }
 }
