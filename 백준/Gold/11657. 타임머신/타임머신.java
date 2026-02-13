1f6764d47c074ab60f7c5222b574fcbae3ef10c8
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-02-13 17:19:09 +0900

    [Gold IV] Title: 타임머신, Time: 232 ms, Memory: 19704 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,66 +1,59 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int n, m;
-	static Edge[] edge;
-	static long[] cost;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		n = sc.nextInt();
-		m = sc.nextInt();
-		cost = new long[n+1];
-		edge = new Edge[m];
-		Arrays.fill(cost, Integer.MAX_VALUE);
-		
-		for(int i = 0; i < m; i++) {
-			int u = sc.nextInt();
-			int v = sc.nextInt();
-			int c = sc.nextInt();
-			
-			edge[i] = new Edge(u, v, c);
-		}
-		
-		boolean flag = bellman_ford(1);
-		
-		if(flag) {
-			System.out.print(-1);
-		} else {
-			StringBuilder sb = new StringBuilder();
-			for(int i = 2; i <= n; i++) {
-				sb.append(cost[i] == Integer.MAX_VALUE ? -1 : cost[i]).append('\n');
-			}
-			System.out.print(sb);
-		}
-	}
-	
-	static boolean bellman_ford(int start) {
-		cost[start] = 0;
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				int u = edge[j].u;
-				int v = edge[j].v;
-				int c = edge[j].c;
-				
-				if(cost[u] != Integer.MAX_VALUE && cost[v] > cost[u] + c) {
-					cost[v] = cost[u] + c;
-					if(i == n - 1) return true;
-				}
-			}
-		}
-		
-		return false;
-	}
-	
-	static class Edge{
-		int u, v, c;
-		Edge(int u, int v, int c){
-			this.u = u;
-			this.v = v;
-			this.c = c;
-		}
-	}
+    static int n, m;
+    static final long INF = Long.MAX_VALUE;
+    static long[] values;
+    static int[][] list;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        list = new int[m][3];
+
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < 3; j++) {
+                list[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        values = new long[n+1];
+        Arrays.fill(values, INF);
+
+        boolean flag = bellman_ford();
+        if(flag) {
+            System.out.print(-1);
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 2; i <= n; i++) {
+                sb.append(values[i] == INF ? -1 : values[i]).append('\n');
+            }
+            System.out.print(sb);
+        }
+    }
+
+    static boolean bellman_ford() {
+        values[1] = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                int from = list[j][0];
+                int to = list[j][1];
+                int cost = list[j][2];
+
+                if(values[from] != INF && values[to] > values[from] + cost) {
+                    values[to] = values[from] + cost;
+                    if(i == n - 1) return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
