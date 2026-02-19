6580ff0a190cf9aa33cf9ebcf6791f0046c2b187
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-02-19 15:47:22 +0900

    [Silver I] Title: 점프, Time: 112 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -1,61 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-/**
- * 백준 1890 점프
- * 메모리 :  KB
- * 시간 :  ms
- *
- */
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    static int N;
+    static int n;
+    static int[][] map;
     static long[][] dp;
-    static int[][] table;
-    static long ans;
-    
-    public static void main(String[] args) throws IOException {
+    static int[] dy = {0, 1};
+    static int[] dx = {1, 0};
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        dp = new long[N][N];
-        table = new int[N][N];
-        
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                table[i][j] = Integer.parseInt(st.nextToken());    
+
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        dp = new long[n][n];
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for(int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
                 dp[i][j] = -1;
             }
         }
-        // 입력 끝
-        ans = dfs(0, 0);
-        System.out.println(ans);
+        long ans = dfs(0, 0);
+
+        System.out.print(ans);
     }
-    
+
     static long dfs(int y, int x) {
-        // 맨 끝에 도착 하면
-        if (y == N-1 && x == N-1) {
+        int value = map[y][x];
+        if(y == n-1 && x == n-1) {
             return 1;
         }
-        if (y >= 0 && y < N && x >= 0 && x < N ) {
-            // 이미 개수를 탐색했던 곳이면
-            if (dp[y][x] != - 1)
-                return dp[y][x];
-            else {
-            	dp[y][x] = 0;
-            	if (y < N) {
-                    dp[y][x] += dfs(y + table[y][x], x);
-            	}
-            	if (x < N) {
-            		dp[y][x] += dfs(y, x + table[y][x]);
-            	}
-                return dp[y][x];
-            	
-            }
+
+        if(dp[y][x] != -1) {
+            return dp[y][x];
         }
-        else {
-        	return 0;
+
+        dp[y][x] = 0;
+        for(int i = 0; i < 2; i++) {
+            int ny = y + dy[i] * value;
+            int nx = x + dx[i] * value;
+
+            if(ny >= n || nx >= n) continue;
+            dp[y][x] += dfs(ny, nx);
         }
+
+        return dp[y][x];
     }
 }
