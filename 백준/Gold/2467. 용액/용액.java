c16b301404c261d2e0f9880e6f7f0b6c8a42897b
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-02-26 17:09:59 +0900

    [Gold V] Title: 용액, Time: 328 ms, Memory: 28380 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,34 +1,55 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n, min = Integer.MAX_VALUE, arr[], res[] = new int[2];
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		//입력
-		n = sc.nextInt();
-		arr = new int[n];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		//입력
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
 		for(int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int l = 0, r = n-1;
+		int min = Integer.MAX_VALUE;
+		int[] resArr = new int[2];
+		int l = 0, r = n - 1;
 		
+//		if(arr[l] >= 0) {
+//			System.out.println(arr[l] + " " + arr[l+1]);
+//			return;
+//		}
+//		
+//		if(arr[r] <= 0) {
+//			System.out.println(arr[r-1] + " " + arr[r]);
+//			return;
+//		}
+//		
+//		for(int i = 0; i < n; i++) {
+//			
+//		}
 		while(l < r) {
-			int tmp = arr[l] + arr[r];
-			int absTmp = tmp < 0 ? -tmp : tmp;
+			int mix = arr[l] + arr[r];
 			
-			if(min > absTmp) {
-				min = absTmp;
-				res[0] = arr[l];
-				res[1] = arr[r];
+//			if(mix == 0) {
+//				System.out.println(arr[l] + " " + arr[r]);
+//				break;
+//			}
+			
+			if(min > Math.abs(mix)) {
+				min = Math.abs(mix);
+				resArr[0] = arr[l];
+				resArr[1] = arr[r];
 			}
 			
-			if(tmp < 0) l++;
+			if(mix < 0) l++;
 			else r--;
 		}
 		
-		System.out.println(res[0] + " " + res[1]);
+		System.out.println(resArr[0] + " " + resArr[1]);
 	}
 }
