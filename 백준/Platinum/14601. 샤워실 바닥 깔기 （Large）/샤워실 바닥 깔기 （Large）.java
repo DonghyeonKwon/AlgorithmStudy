a36b34c0c76c4b0288f7ed3d0dee1e317b1f38fb
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-03-02 13:53:00 +0900

    [Platinum V] Title: 샤워실 바닥 깔기 (Large), Time: 132 ms, Memory: 24388 KB -BaekjoonHub

--- 백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.java
+++ 백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.java
@@ -7,12 +7,14 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int k = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split(" ");
-        x = Integer.parseInt(input[0]);
-        y = Integer.parseInt(input[1]);
 
-        n = pow(k);
+        n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+
+        n = (int) Math.pow(2, n);
         map = new int[n+1][n+1];
         y = n - y + 1;
         map[y][x] = -1;
@@ -22,43 +24,38 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= n; j++) {
-                sb.append(map[i][j] + " ");
+                sb.append(map[i][j]).append(' ');
             }
             sb.append('\n');
         }
-        sb.append('\n');
 
         System.out.print(sb);
     }
 
-    static void go(int y, int x, int size) {
+    static void go(int sy, int sx,  int size) {
         cnt++;
 
-        int hs = size / 2;
-        if(check(y, x, hs)) map[y+hs-1][x+hs-1] = cnt;
-        if(check(y+hs, x, hs)) map[y+hs][x+hs-1] = cnt;
-        if(check(y, x+hs, hs)) map[y+hs-1][x+hs] = cnt;
-        if(check(y+hs, x+hs, hs)) map[y+hs][x+hs] = cnt;
+        int half = size / 2;
+
+        if(check(sy, sx, half)) map[sy + half - 1][sx + half - 1] = cnt;
+        if(check(sy + half, sx, half)) map[sy + half][sx + half - 1] = cnt;
+        if(check(sy, sx + half, half)) map[sy + half - 1][sx + half] = cnt;
+        if(check(sy + half, sx + half, half)) map[sy + half][sx + half] = cnt;
 
         if(size == 2) return;
-        go(y, x, hs);
-        go(y, x + hs, hs);
-        go(y + hs, x, hs);
-        go(y + hs, x + hs, hs);
+
+        go(sy, sx, half);
+        go(sy + half, sx, half);
+        go(sy, sx + half, half);
+        go(sy + half, sx + half, half);
     }
 
-    static boolean check(int y, int x, int size) {
-        for(int i = y; i < y + size; i++) {
-            for(int j = x; j < x + size; j++) {
+    static boolean check(int y, int x, int s) {
+        for(int i = y; i < y + s; i++) {
+            for(int j = x; j < x + s; j++) {
                 if(map[i][j] != 0) return false;
             }
         }
-
         return true;
-    }
-
-    static int pow(int k) {
-        if(k == 1) return 2;
-        return pow(k/2) * pow(k/2) * ((k % 2 == 1) ? 2 : 1);
     }
 }
