7122c12c1162dceea0f72f96b781afada875d3be
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-03-08 20:59:46 +0900

    [Gold II] Title: 게리맨더링 2, Time: 296 ms, Memory: 32252 KB -BaekjoonHub

--- 백준/Gold/17779. 게리맨더링 2/게리맨더링 2.java
+++ 백준/Gold/17779. 게리맨더링 2/게리맨더링 2.java
@@ -1,91 +1,98 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int n, y, x, total = 0, res = 999999999;
-	static int[][] map;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		
-		map = new int[n+1][n+1];
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= n; j++) {
-				map[i][j] = sc.nextInt();
-				total += map[i][j];
-			}
-		}
-		
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= n; j++) {
-				for(int d1 = 1; d1 <= n-2; d1++) {
-					for(int d2 = 1; d2 <= n-2; d2++) {
-						go(i, j, d1, d2);
-					}
-				}
-			}
-		}
-		
-		System.out.println(res);
-	}
-	
-	static void go(int x, int y, int d1, int d2) {
-		if(x + d1 + d2 > n) return;
-		if(x >= x + d1 + d2) return;
-		if(y - d1 < 1 || y + d2 > n) return;
-		
-		boolean[][] visited = new boolean[n+1][n+1];
-		for(int i = 0; i <= d1; i++) {
-			visited[x + i][y - i] = true;
-			visited[x + d2 + i][y + d2 - i] = true;
-		}
-		
-		for(int i = 0; i <= d2; i++) {
-			visited[x + i][y + i] = true;
-			visited[x + d1 + i][y - d1 + i]=  true;
-		}
-		
-		int[] sum = new int[6];
-		sum[5] = total;
-		for(int i = 1; i < x + d1; i++) {
-			for(int j = 1; j <= y; j++) {
-				if(visited[i][j]) break;
-				sum[1] += map[i][j];
-				sum[5] -= map[i][j];
-			}
-		}
-		
-		for(int i = 1; i <= x + d2; i++) {
-			for(int j = n; j > y; j--) {
-				if(visited[i][j]) break;
-				sum[2] += map[i][j];
-				sum[5] -= map[i][j];
-			}
-		}
-		
-		for(int i = x+d1; i <= n; i++) {
-			for(int j = 1; j < y - d1 + d2; j++) {
-				if(visited[i][j]) break;
-				sum[3] += map[i][j];
-				sum[5] -= map[i][j];
-			}
-		}
-		
-		for(int i = x + d2 + 1; i <= n; i++) {
-			for(int j = n; j >= y - d1 + d2; j--) {
-				if(visited[i][j]) break;
-				sum[4] += map[i][j];
-				sum[5] -= map[i][j];
-			}
-		}
+    static int n, total = 0, min = 987654321;
+    static int[][] arr;
 
-		int max = 0;
-		int min = 999999999;
-		for(int i = 1; i <= 5; i++) {
-			max = Math.max(sum[i], max);
-			min = Math.min(sum[i], min);
-		}
-		
-		res = Math.min(max - min, res);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                total += arr[i][j];
+            }
+        }
+
+        for(int y = 0; y < n; y++) {
+            for(int x = 0; x < n; x++) {
+                for(int d1 = 1; d1 <= n - 2; d1++) {
+                    for(int d2 = 1; d2 <= n - 2; d2++) {
+                        if(y + d1 + d2 >= n || x + d2 >= n || x - d1 < 0) continue;
+                        check(y, x, d1, d2);
+                    }
+                }
+            }
+        }
+
+        System.out.print(min);
+    }
+
+    static void check(int y, int x, int d1, int d2) {
+        boolean[][] visited = new boolean[n][n];
+        for(int i = 0; i <= d1; i++) {
+            visited[y + i][x - i] = true;
+            visited[y + d2 + i][x + d2 - i] = true;
+        }
+
+        for(int i = 0; i <= d2; i++) {
+            visited[y+i][x+i] = true;
+            visited[y+d1+i][x-d1+i] = true;
+        }
+
+        int[] sum = new int[6];
+        sum[5] = total;
+
+        //1번
+        for(int i = 0; i < y + d1; i++) {
+            for(int j = 0; j <= x; j++) {
+                if(visited[i][j]) break;
+                sum[1] += arr[i][j];
+                sum[5] -= arr[i][j];
+            }
+        }
+
+        //2번
+        for(int i = 0; i <= y + d2; i++) {
+            for(int j = n-1; j > x; j--) {
+                if(visited[i][j]) break;
+                sum[2] += arr[i][j];
+                sum[5] -= arr[i][j];
+            }
+        }
+
+        //3번
+        for(int i = n-1; i >= y + d1; i--) {
+            for(int j = 0; j < x - d1 + d2; j++) {
+                if(visited[i][j]) break;
+                sum[3] += arr[i][j];
+                sum[5] -= arr[i][j];
+            }
+        }
+
+        //4번
+        for(int i = n-1; i > y + d2; i--) {
+            for(int j = n-1; j >= x - d1 + d2; j--) {
+                if(visited[i][j]) break;
+                sum[4] += arr[i][j];
+                sum[5] -= arr[i][j];
+            }
+        }
+
+
+        int mx = 0;
+        int mn = 987654321;
+        for(int i = 1; i <= 5; i++) {
+            mx = Math.max(mx, sum[i]);
+            mn = Math.min(mn, sum[i]);
+        }
+
+        if(min > mx - mn) {
+            min = mx - mn;
+        }
+    }
 }
