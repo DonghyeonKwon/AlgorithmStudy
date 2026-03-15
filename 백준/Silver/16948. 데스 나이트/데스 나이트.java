3a60504a4c4bd6aa3783049fb5f7fef3242fea3d
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-03-15 12:49:09 +0900

    [Silver I] Title: 데스 나이트, Time: 112 ms, Memory: 14648 KB -BaekjoonHub

--- 백준/Silver/16948. 데스 나이트/데스 나이트.java
+++ 백준/Silver/16948. 데스 나이트/데스 나이트.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int[][] visited;
     static int[] dy = {-2, -2, 0, 0, 2, 2};
     static int[] dx = {-1, 1, -2, 2, -1, 1};
 
@@ -11,38 +10,39 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
-        visited = new int[n][n];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r1 = Integer.parseInt(st.nextToken());
+        int c1 = Integer.parseInt(st.nextToken());
+        int r2 = Integer.parseInt(st.nextToken());
+        int c2 = Integer.parseInt(st.nextToken());
+
+        System.out.print(bfs(r1, c1, r2, c2));
+    }
+
+    static int bfs(int r1, int c1, int r2, int c2) {
+        Queue<int[]> q = new ArrayDeque<>();
+        int[][] visited = new int[n][n];
         for(int i = 0; i < n; i++) {
             Arrays.fill(visited[i], Integer.MAX_VALUE);
         }
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int sy = Integer.parseInt(st.nextToken());
-        int sx = Integer.parseInt(st.nextToken());
-        int ey = Integer.parseInt(st.nextToken());
-        int ex = Integer.parseInt(st.nextToken());
-
-        bfs(sy, sx, ey, ex);
-
-        System.out.print(visited[ey][ex] == Integer.MAX_VALUE ? -1 : visited[ey][ex]);
-    }
-
-    static void bfs(int sy, int sx, int ey, int ex) {
-        Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{sy, sx});
-        visited[sy][sx] = 0;
+        q.add(new int[] {r1, c1});
+        visited[r1][c1] = 0;
 
         while(!q.isEmpty()) {
             int[] pos = q.poll();
+
             int y = pos[0];
             int x = pos[1];
 
-            if(ey == y && ex == x) break;
+            if(y == r2 && x == c2) {
+                return visited[y][x];
+            }
 
-            for(int d = 0; d < 6; d++) {
-                int ny = y + dy[d];
-                int nx = x + dx[d];
+            for(int i = 0; i < 6; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
 
                 if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
                 if(visited[ny][nx] <= visited[y][x] + 1) continue;
@@ -50,5 +50,7 @@ public class Main {
                 q.add(new int[]{ny, nx});
             }
         }
+
+        return -1;
     }
 }
