a2e35e0a3fdc91f6425bcc25bd94f4fa256aad98
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-03-20 14:06:03 +0900

    [Gold IV] Title: 전구와 스위치, Time: 156 ms, Memory: 27540 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
@@ -2,49 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, ans = -1;
+    static int n, res = -1;
     static char[] input, result;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
 
+        n = Integer.parseInt(br.readLine());
         input = br.readLine().toCharArray();
         result = br.readLine().toCharArray();
 
-        char[] temp = new char[n];
-        for(int i = 0; i < n; i++) temp[i] = input[i];
+        char[] tmp = Arrays.copyOf(input, n);
 
-        go(0, 1);
-        if(ans == -1) {
-            input = temp;
+        go(1, 0);
+        if(res == -1) {
+            input = tmp;
             input[0] = change(input[0]);
             input[1] = change(input[1]);
             go(1, 1);
         }
 
-        System.out.print(ans);
+        System.out.print(res);
     }
 
-    static void go(int cnt, int x) {
-        if(x == n) {
-            if(input[x-1] == result[x-1]) {
-                if(ans == -1) ans = cnt;
-                else ans = Math.min(cnt, ans);
-            }
+    static void go(int idx, int cnt) {
+        if(idx == n) {
+            if(input[idx - 1] != result[idx - 1]) return;
+            if(res == -1) res = cnt;
+            if(res > cnt) res = cnt;
             return;
         }
 
-        if(input[x-1] == result[x-1]) go(cnt, x + 1);
-        else {
-            input[x-1] = change(input[x-1]);
-            input[x] = change(input[x]);
-            if(x + 1 != n) input[x+1] = change(input[x+1]);
-            go(cnt + 1, x + 1);
+        if(input[idx - 1] != result[idx - 1]) {
+            input[idx - 1] = change(input[idx-1]);
+            input[idx] = change(input[idx]);
+            if(idx + 1 < n) input[idx + 1] = change(input[idx + 1]);
+            go(idx + 1, cnt + 1);
+        } else {
+            go(idx + 1, cnt);
         }
     }
 
     static char change(char c) {
-        return c == '1' ? '0' : '1';
+        return c == '0' ? '1' : '0';
     }
 }
