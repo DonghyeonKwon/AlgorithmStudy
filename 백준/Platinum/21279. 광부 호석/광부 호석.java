11e716f5abb4eab6cf1ec3fc31d91a7dd7c8db00
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-03-21 13:21:21 +0900

    [Platinum V] Title: 광부 호석, Time: 580 ms, Memory: 88376 KB, Score: 16 point -BaekjoonHub

--- 백준/Platinum/21279. 광부 호석/광부 호석.java
+++ 백준/Platinum/21279. 광부 호석/광부 호석.java
@@ -2,71 +2,97 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, c;
-    static List<Info> list = new ArrayList<>();
-    static List<Info>[] arrList;
+    static int n, m;
+    static long[] cntTree;
+    static long[] sumTree;
+    static Map<Integer, List<int[]>> map;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        int maxY = 0, maxX = 0;
+        m = Integer.parseInt(st.nextToken());
+
+        cntTree = new long[400001];
+        sumTree = new long[400001];
+        map = new HashMap<>();
+
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int y = Integer.parseInt(st.nextToken());
+
             int x = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int val = Integer.parseInt(st.nextToken());
 
-            maxY = Math.max(y, maxY);
-            maxX = Math.max(x, maxX);
-
-            list.add(new Info(y, x, cost));
+            if(map.containsKey(x)) {
+                map.get(x).add(new int[]{y, val});
+            } else {
+                List<int[]> list = new ArrayList<>();
+                list.add(new int[]{y, val});
+                map.put(x, list);
+            }
         }
 
-        arrList = new ArrayList[maxX + 1];
-        for(int i = 0; i <= maxX; i++) arrList[i] = new ArrayList<>();
-        for(Info k : list) {
-            arrList[k.x].add(k);
-        }
+        long ans = 0;
+        for(int i = 0; i <= 100000; i++) {
+            if(map.containsKey(i)) {
 
-        long ans = 0, sum = 0;
-        int nowY = maxY;
-        PriorityQueue<Info> pq = new PriorityQueue<>((a, b) -> b.y - a.y);
-
-        for(int x = 0; x <= maxX; x++) {
-            if(arrList[x].isEmpty()) continue;
-
-            for(Info i : arrList[x]) {
-                if(i.y <= nowY) {
-                    pq.add(i);
-                    sum += i.cost;
+                for(int[] arr : map.get(i)) {
+                    update(1, 0, 100000, arr[0], arr[1]);
                 }
-            }
 
-            while(pq.size() > c) {
-                int max = pq.peek().y;
+                long totalSum = sumTree[1];
+                long totalCnt = cntTree[1];
 
-                while(!pq.isEmpty() && max == pq.peek().y) {
-                    sum -= pq.poll().cost;
+
+                if(totalCnt <= m) {
+                    ans = Math.max(ans, totalSum);
+                } else {
+                    int pos = kth(1, 0, 100000, m + 1);
+                    int h = pos - 1;
+
+                    if(h >= 0) {
+                        long sum = getSum(1, 0, 100000, 0, h);
+                        ans = Math.max(sum, ans);
+                    }
                 }
-                nowY = max;
+                
             }
-
-            ans = Math.max(ans, sum);
         }
 
         System.out.print(ans);
     }
 
-    static class Info {
-        int y, x, cost;
+    static void update(int node, int l, int r, int idx, int val) {
+        if(idx < l || idx > r) return;
 
-        Info(int y, int x, int cost) {
-            this.y = y;
-            this.x = x;
-            this.cost = cost;
+        if(l == r) {
+            cntTree[node] += 1;
+            sumTree[node] += val;
+            return;
         }
+
+        int mid = (l + r) / 2;
+        update(node * 2, l, mid, idx, val);
+        update(node * 2 + 1, mid + 1, r, idx, val);
+
+        cntTree[node] = cntTree[node * 2] + cntTree[node * 2 + 1];
+        sumTree[node] = sumTree[node * 2] + sumTree[node * 2 + 1];
+    }
+
+    static int kth(int node, int l, int r, long cnt) {
+        if(l == r) return l;
+        int mid = (l + r) / 2;
+        long lCnt = cntTree[node * 2];
+        if(cnt <= lCnt) return kth(node * 2, l, mid, cnt);
+        else return kth(node * 2 + 1, mid + 1, r, cnt - lCnt);
+    }
+
+    static long getSum(int node, int l, int r, int ll, int rr) {
+        if(rr < l || ll > r) return 0;
+        if(ll <= l && r <= rr) return sumTree[node];
+        int mid = (l + r) / 2;
+        return getSum(node * 2, l, mid, ll, rr) + getSum(node * 2 + 1, mid + 1, r, ll, rr);
     }
 }
