61fc5201dd8fd24af5749897d4e2d9ad9e2fdfaa
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-03-21 13:21:10 +0900

    [Platinum V] Title: 광부 호석, Time: 512 ms, Memory: 90640 KB, Score: 16 point -BaekjoonHub

--- 백준/Platinum/21279. 광부 호석/광부 호석.java
+++ 백준/Platinum/21279. 광부 호석/광부 호석.java
@@ -3,90 +3,70 @@ import java.util.*;
 
 public class Main {
     static int n, c;
-    static List<int[]>[] listByX;
-
-    static long[] segCnt = new long[400001];
-    static long[] segSum = new long[400001];
-
+    static List<Info> list = new ArrayList<>();
+    static List<Info>[] arrList;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        listByX = new ArrayList[100001];
-        for(int i = 0; i <= 100000; i++) listByX[i] = new ArrayList<>();
-
         n = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-
+        int maxY = 0, maxX = 0;
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-
-            int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-            listByX[x].add(new int[]{y, v});
+            maxY = Math.max(y, maxY);
+            maxX = Math.max(x, maxX);
+
+            list.add(new Info(y, x, cost));
         }
 
-        long ans = 0;
+        arrList = new ArrayList[maxX + 1];
+        for(int i = 0; i <= maxX; i++) arrList[i] = new ArrayList<>();
+        for(Info k : list) {
+            arrList[k.x].add(k);
+        }
 
-        for(int x = 0; x <= 100000; x++) {
-            for(int[] p : listByX[x]) {
-                int y = p[0];
-                int v = p[1];
-                update(1, 0, 100000, y, 1, v);
-            }
+        long ans = 0, sum = 0;
+        int nowY = maxY;
+        PriorityQueue<Info> pq = new PriorityQueue<>((a, b) -> b.y - a.y);
 
-            long totalCnt = segCnt[1];
-            long totalSum = segSum[1];
+        for(int x = 0; x <= maxX; x++) {
+            if(arrList[x].isEmpty()) continue;
 
-            if(totalCnt <= c) {
-                ans = Math.max(ans, totalSum);
-            } else {
-                int pos = kth(1, 0, 100000, c + 1);
-                int h = pos - 1;
-
-                if(h >= 0) {
-                    long s = querySum(1, 0, 100000, 0, h);
-                    ans = Math.max(ans, s);
-                } else {
-                    ans = Math.max(ans, 0);
+            for(Info i : arrList[x]) {
+                if(i.y <= nowY) {
+                    pq.add(i);
+                    sum += i.cost;
                 }
             }
+
+            while(pq.size() > c) {
+                int max = pq.peek().y;
+
+                while(!pq.isEmpty() && max == pq.peek().y) {
+                    sum -= pq.poll().cost;
+                }
+                nowY = max;
+            }
+
+            ans = Math.max(ans, sum);
         }
 
         System.out.print(ans);
     }
 
-    static void update(int node, int l, int r, int idx, int cnt, int sum) {
-        if(idx < l || r < idx) return;
+    static class Info {
+        int y, x, cost;
 
-        if(l == r) {
-            segCnt[node] += cnt;
-            segSum[node] += sum;
-            return;
+        Info(int y, int x, int cost) {
+            this.y = y;
+            this.x = x;
+            this.cost = cost;
         }
-
-        int mid = (l + r) / 2;
-        update(node * 2, l, mid, idx, cnt, sum);
-        update(node * 2 + 1, mid + 1, r, idx, cnt, sum);
-        segCnt[node] = segCnt[node * 2] + segCnt[node * 2 + 1];
-        segSum[node] = segSum[node * 2] + segSum[node * 2 + 1];
-    }
-
-    static int kth(int node, int l, int r, long k) {
-        if(l == r) return l;
-        int mid = (l + r) / 2;
-        long leftCnt = segCnt[node * 2];
-        if(k <= leftCnt) return kth(node * 2, l, mid, k);
-        return kth(node * 2 + 1, mid + 1, r, k - leftCnt);
-    }
-
-    static long querySum(int node, int l, int r, int ql, int qr) {
-        if(qr < l || ql > r) return 0;
-        if(ql <= l && r <= qr) return segSum[node];
-        int mid = (l + r) / 2;
-        return querySum(node * 2, l, mid, ql, qr) + querySum(node * 2 + 1, mid + 1, r, ql, qr);
     }
 }
