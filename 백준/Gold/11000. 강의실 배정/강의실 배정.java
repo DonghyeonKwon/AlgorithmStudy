29cf6b67c1fc8ea395e5c571f8f40f665caa881e
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-03-23 15:58:40 +0900

    [Gold IV] Title: 강의실 배정, Time: 624 ms, Memory: 62444 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -5,33 +5,36 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
+        int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][2];
-        for(int i = 0; i < n; i++){
+
+        for(int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < 2; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        Arrays.sort(arr, (i, j) -> {
-            if(i[0] == j[0]) return i[1] - j[1];
-            return i[0] - j[0];
-        });
+        Arrays.sort(arr,
+                (a, b) -> {
+                    if(a[0] == b[0]) return a[1] - b[1];
+                    return a[0] - b[0];
+                }
+        );
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.offer(arr[0][1]);
+        pq.add(arr[0][1]);
 
-        for(int i = 1; i < n; i++){
-            int a = pq.peek();
-            if(arr[i][0] < a) pq.offer(arr[i][1]);
-            else {
+        for(int i = 1; i < n; i++) {
+            if(pq.peek() > arr[i][0]) {
+                pq.add(arr[i][1]);
+            } else {
                 pq.poll();
-                pq.offer(arr[i][1]);
+                pq.add(arr[i][1]);
             }
         }
 
         System.out.print(pq.size());
-
     }
 }
