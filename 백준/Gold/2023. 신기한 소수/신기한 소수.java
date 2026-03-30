20fbf61ef0970cd004110ac611601806d394c317
ddh24 <93506933+DonghyeonKwon@users.noreply.github.com>
2026-03-30 21:37:45 +0900

    [Gold V] Title: 신기한 소수, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,40 +1,44 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int n;
-	static StringBuilder sb = new StringBuilder();
-	static int[] firstNum = { 2, 3, 5, 7 };
+    static int n;
+    static StringBuilder sb = new StringBuilder();
+    static int[] firstNum = {2, 3, 5, 7};
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		go(0, 0);
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	static void go(int idx, int num) {
-		if (n == idx) {
-			sb.append(num).append("\n");
-			return;
-		}
+        n = Integer.parseInt(br.readLine());
+        go(0, 0);
 
-		if (idx == 0) {
-			for(int x : firstNum) {
-				go(idx+1, x);
-			}
-		} else {
-			for(int i = 1; i <= 9; i+=2) {
-				if(isPrime(num * 10 + i)) {
-					go(idx+1, num*10 + i);
-				}
-			}
-		}
-	}
-	
-	static boolean isPrime(int k) {
-		for(int i = 2; i <= (int)Math.sqrt(k); i++) {
-			if(k % i == 0) return false;
-		}
-		return true;
-	}
+        System.out.print(sb);
+    }
+    
+    static void go(int idx, int num) {
+        if(n == idx) {
+            sb.append(num).append('\n');
+            return;
+        }
+
+        if(idx == 0) {
+            for(int x : firstNum) {
+                go(idx + 1, x);
+            }
+        } else {
+            for(int i = 1; i <= 9; i += 2) {
+                if(isPrime(num * 10 + i)) {
+                    go(idx + 1, num * 10 + i);
+                }
+            }
+        }
+    }
+
+    static boolean isPrime(int k) {
+        for(int i = 2; i < (int)Math.sqrt(k) + 1; i++) {
+            if(k % i == 0) return false;
+        }
+
+        return true;
+    }
 }
